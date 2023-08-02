bc622b024a3cc9cc66aa1b3a8b605459d42c9780
omidoyin--Git <omidoyinayodeji@yahoo.com>
2023-08-02 13:47:36 +0100

    fixing errors

--- src/data/data.js
+++ src/data/data.js
@@ -2,9 +2,9 @@ import img1 from "../assets/backend.jpg";
 import img2 from "../assets/block.jpg";
 import img3 from "../assets/web.jpg";
 import img4 from "../assets/cloud.jpg";
-import img5 from "../assets/cloud.jpg";
+import img5 from "../assets/ai2.jpg";
 import img6 from "../assets/xr.jpg";
-import img7 from "../assets/scratch.JPG";
+import img7 from "../assets/scratch2.png";
 import img8 from "../assets/automation.jpg";
 
 export const data = [
